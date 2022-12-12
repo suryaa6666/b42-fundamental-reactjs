26967239725c0871709cbdfb15ee64954809cddf
SuryaElz <suryaelidanto@gmail.com>
2022-12-12 21:40:35 +0700

    code result

---
+++ src/component.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import component Header
+
+function Component() {
+  return (
+    //   Code Here
+    <div></div>
+  );
+}
+
+// Create a new component here
+
+export default Component;
